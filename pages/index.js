b8c8f3fab099b17b67a16e1c578b4ba7930227f6
naotoiso <naotoiso@amazon.co.jp>
2023-07-29 23:16:45 +0000

    git deploy

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import useSWR from 'swr'
 import Person from '../components/Person'
+import { useRouter } from 'next/router'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
+  const router = useRouter()
   const { data, error } = useSWR('/api/people', fetcher)
 
   if (error) return <div>Failed to load</div>
@@ -17,8 +19,11 @@ export default function Index() {
       ))}
     </ul>
     <Link href="/person" as={`/`}>
-      getServerSidePropsに戻る
+    Link で getServerSidePropsに戻る
     </Link>
+    <button onClick={() => router.push('/person')}>
+    useRouter で ServerSidePropsに戻る
+    </button>
 
   </>
   )
